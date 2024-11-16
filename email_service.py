c1b793742201862e5f456efeb8af942e95018c7d
Juan Ibarra <jmibarra86@gmail.com>
2024-11-16 19:38:43 -0300

    Modifico para tener multiples productos

--- email_service.py
+++ email_service.py
@@ -1,11 +1,13 @@
 # email_service.py
 import smtplib
 from email.mime.text import MIMEText
+#### **email_service.py**
+from config import EMAIL, PASSWORD
 
 class EmailService:
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
+    def __init__(self):
+        self.email = EMAIL
+        self.password = PASSWORD
 
     def send_email(self, subject, body, recipient_email):
         msg = MIMEText(body)
--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 from email_service import EmailService
 
-#URL del producto
-URL = 'https://www.mercadolibre.com.ar/toyco-juego-de-la-vida-life/p/MLA15904269'
+#URLs del producto
+# Lista de productos
+products = [
+    {"name": "Juego de la vida", "url": "https://www.mercadolibre.com.ar/toyco-juego-de-la-vida-life/p/MLA15904269", "target_price": 39901.00},
+    {"name": "Play station 5", "url": "https://www.mercadolibre.com.ar/sony-playstation-5-825gb-god-of-war-ragnarok-bundle-color-blanconegro/p/MLA19917557", "target_price": 10000000.00},
+]
 TARGET_PRICE = 39901.00  # Precio objetivo
 
+from config import RECIPENT_EMAIL 
+
 # Crea una instancia del servicio de correo
-email_service = EmailService(email=EMAIL, password=PASSWORD)
+email_service = EmailService()
 
 # Cabeceras para imitar un navegador real
 HEADERS = {
@@ -28,10 +34,23 @@ def get_product_price(url):
 
     return title, price
 
-def check_price(title, price):
-    if price < TARGET_PRICE:
-        body = f"¡El precio del producto '{title}' ha bajado a ${price}!\nLink: {URL}"
-        email_service.send_email("¡Alerta de precio bajo!", body, recipient_email=EMAIL)
 
-title, price = get_product_price(URL)
-check_price(title, price)
+def track_prices(products):
+    for product in products:
+        try:
+            title, price = get_product_price(product["url"])
+            print(f"Producto: {title} | Precio actual: ${price}")
+
+            # Verifica si el precio está por debajo del objetivo
+            if price <= product["target_price"]:
+                body = (
+                    f"¡El precio del producto '{title}' ha bajado a ${price}!\n"
+                    f"Link: {product['url']}"
+                )
+                email_service.send_email(
+                    f"Alerta de precio bajo: {title}", body, recipient_email=RECIPENT_EMAIL
+                )
+        except Exception as e:
+            print(f"Error rastreando {product['name']}: {e}")
+
+track_prices(products)
